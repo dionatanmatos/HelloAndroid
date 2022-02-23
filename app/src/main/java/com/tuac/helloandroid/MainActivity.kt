9f5fa6f98964dbf4519c3e12dcb78781d9b78c15
Dionatan Matos <matos.dionatan@gmail.com>
2022-02-22 22:24:23 -0300

    Alteração teste 1

--- app/src/main/java/com/tuac/helloandroid/MainActivity.kt
+++ app/src/main/java/com/tuac/helloandroid/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tuac.helloandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//teste1
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
